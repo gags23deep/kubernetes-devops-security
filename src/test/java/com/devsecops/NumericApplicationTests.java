0118160568922dc00cb46b0c6e7cb02e1c4664f6
gags23deep <62713829+gags23deep@users.noreply.github.com>
2022-12-07 19:09:00 -0500

    update

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -46,10 +46,12 @@ public class NumericApplicationTests {
                 .andExpect(content().string("Greater than 50"));
     }
     
-    @Test
+    
+
+@Test
     public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-         .andExpect(content().string("kubernetes DevSecOps"));
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("Kubernetes DevSecOps"));
     }
     
 
